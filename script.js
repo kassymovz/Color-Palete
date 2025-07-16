dbe5a22c40c187045de9a8e94ab3881a50fd7a58
kassymovz <kassymovz@bk.ru>
2025-07-16 21:48:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function red(){
+    document.getElementById("color").style.background = "red";
+}
+function orange(){
+    document.getElementById("color").style.background = "orange";
+}
+function yellow(){
+    document.getElementById("color").style.background = "yellow";
+}
+function green(){
+    document.getElementById("color").style.background = "green";
+}
+function cyan(){
+    document.getElementById("color").style.background = "cyan";
+}
+function blue(){
+    document.getElementById("color").style.background = "blue";
+}
+function purple(){
+    document.getElementById("color").style.background = "purple";
+}
+function brown(){
+    document.getElementById("color").style.background = "brown";
+}
+function pink(){
+    document.getElementById("color").style.background = "pink";
+}
+function silver(){
+    document.getElementById("color").style.background = "silver";
+}
+function gold(){
+    document.getElementById("color").style.background = "gold";
+}
+function wheat(){
+    document.getElementById("color").style.background = "wheat";
+}
+function ClearDisplay(){
+    document.getElementById("null").style.visibility = "hidden";
+}
+function Visibile(){
+    document.getElementById("null").style.visibility = "visible";
+}
